d3921a3775aaadfbdba25841ec6d5435cd4a771a
waynegakuo <waynejotham@gmail.com>
2019-08-18 23:42:32 +0300

    Learning how to generate components using the CLI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; // importing compmonent package from angular core
 
+// this is a decorator that includes metadata of the component
+// selector is what will be used in the directive/html element in index.html
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-crash-todolist';
 }
+
+  // name = 'Wayne Gakuo';
+  // constructor() {
+  //   this.changeName( 'John');
+  // }
+  // changeName(name: string): void {
+  //   this.name = name;}
