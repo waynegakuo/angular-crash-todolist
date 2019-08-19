459a917ee0e9a63590875f423f8b3f866a47bfce
waynegakuo <waynejotham@gmail.com>
2019-08-20 01:49:09 +0300

    using the routes file to set the todos as the root

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TodosComponent } from './components/todos/todos.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: TodosComponent} // we want the TodosComponent to be our root
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
