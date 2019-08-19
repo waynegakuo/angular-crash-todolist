839963f1f87b66c42d1c408c1404a7353582cfb0
waynegakuo <waynejotham@gmail.com>
2019-08-20 02:04:10 +0300

    Generated an About page component to serve as a static page. Was able to route it and added a nav bar to the header component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TodosComponent } from './components/todos/todos.component';
+import { AboutComponent } from './components/pages/about/about.component';
 
 
 const routes: Routes = [
-  {path: '', component: TodosComponent} // we want the TodosComponent to be our root
+  {path: '', component: TodosComponent}, // we want the TodosComponent to be our root
+  {path: 'about', component: AboutComponent } // the AboutComponent is another page route
 ];
 
 @NgModule({
