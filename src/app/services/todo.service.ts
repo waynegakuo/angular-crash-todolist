f45a2ebbd4532596c0b30a993cc5dccf5255e0b5
waynegakuo <waynejotham@gmail.com>
2019-08-19 22:50:04 +0300

    Gets data from JSON Placeholder and receives it as an observable of type Todos[] and we subscribe to it

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Todo } from '../models/Todo'; // imported model for use in this services.ts file
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  todosUrl: 'https://jsonplaceholder.typicode.com/todos';
 
-  constructor() { }
+  constructor(private http: HttpClient ) { }
+
+  getTodos(): Observable<Todo[]> {
+    // getting an http request & making it a Todo[] item (arrays of type Todo)
+    return this.http.get<Todo[]>(this.todosUrl);
+  }
 }
